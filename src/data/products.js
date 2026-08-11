8d455b88bdca35d04d23e22863dbb8b1f0b1507e
munirnsyh11 <242310027@student.ibik.ac.id>
2026-08-11 15:27:09 +0700

    Fix product images

--- src/data/products.js
+++ src/data/products.js
@@ -1,11 +1,15 @@
+import baju1 from "../assets/products/baju1.jpg";
+import baju2 from "../assets/products/baju2.jpg";
+
 const products = [
   {
     id: "valley-heritage-tee",
     name: "Heritage Valley Tee",
     category: "T-Shirt",
     price: "Rp 130.000",
-    image: "../src/assets/products/baju1.jpg",
-    description: "Kaos katun combed 24s dengan sablon line-art valley di dada kiri.",
+    image: baju1,
+    description:
+      "Kaos katun combed 24s dengan sablon line-art valley di dada kiri.",
     featured: true,
     marketplaceUrl: "#",
   },
@@ -14,8 +18,9 @@ const products = [
     name: "Ridge Overshirt",
     category: "Outerwear",
     price: "Rp 349.000",
-    image: "../src/assets/products/baju2.jpg",
-    description: "Overshirt katun tebal dengan warna earthy brown, cocok untuk udara pegunungan.",
+    image: baju2,
+    description:
+      "Overshirt katun tebal dengan warna earthy brown, cocok untuk udara pegunungan.",
     featured: true,
     marketplaceUrl: "#",
   },
